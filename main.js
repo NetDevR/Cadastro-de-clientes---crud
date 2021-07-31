5f777c7e0f6c1c9e0d638624094f247cb04edd4f
Ednaldo Neto <ednaldonetoaraujo18@gmail.com>
2021-07-31 17:18:28 -0300

    .

--- main.js
+++ main.js
@@ -6,6 +6,29 @@ const openModal = () => document.getElementById('modal')
 const closeModal = () => document.getElementById('modal')
     .classList.remove('active')
 
+
+const tempCliente = {
+    nome: "Clebinho",
+    email: 'clebinho@yahoo.com.br',
+    celular: '40028922',
+    cidade: 'capela'
+
+}
+
+// Início do CRUD - Create
+
+const createCliente = (cliente) => {
+    const db_clients = JSON.parse(localStorage.getItem('db_clients'))
+    db_clients.push (cliente)
+    localStorage.setItem('db_cliente', JSON.stringify(db_clients))
+
+}
+
+
+
+
+
+
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
