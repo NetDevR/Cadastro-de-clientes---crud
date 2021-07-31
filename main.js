fceb1e51fc16cf31bef0dd593322a158ef00565d
Ednaldo Neto <ednaldonetoaraujo18@gmail.com>
2021-07-31 17:56:26 -0300

    crud completo

--- main.js
+++ main.js
@@ -8,29 +8,67 @@ const closeModal = () => document.getElementById('modal')
 
 
 const tempCliente = {
-    nome: "Clebinho",
-    email: 'clebinho@yahoo.com.br',
-    celular: '40028922',
-    cidade: 'capela'
+    nome: "Jorge",
+    email: 'jorgin@yahoo.com.br',
+    celular: '34536221',
+    cidade: 'colegio'
 
 }
 
-// Início do CRUD - Create
+const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
+const setLocalStorage = (db_client) => localStorage.setItem('db_client', JSON.stringify(db_client))
 
-const createCliente = (cliente) => {
-    const db_clients = JSON.parse(localStorage.getItem('db_clients'))
-    db_clients.push (cliente)
-    localStorage.setItem('db_cliente', JSON.stringify(db_clients))
+// Início do CRUD 
 
+// Create
+
+const createCliente = (client) => {
+    const db_client = getLocalStorage()
+    db_client.push (client)
+    setLocalStorage(db_client)
+
+}
+
+// Read
+
+const readCliente = () => getLocalStorage()
+
+// Update
+
+const updateCliente = (index, client) => {
+    const db_cliente = readCliente()
+    db_cliente[index] = client
+    setLocalStorage(db_cliente)
+
+}
+
+// Delete
+
+const deleteCliente = (index) => {
+    db_cliente = readCliente()
+    db_client.slice(index,1)
+    setLocalStorage(db_client)
+}
+
+// Interação cm o layout
+
+const saveClient = () =>{
+    if (isValidFields()){
+        console.log('Cadastrando cliente')
+
+    }
 }
 
 
 
 
 
-
+//Eventos
 document.getElementById('cadastrarCliente')
     .addEventListener('click', openModal)
 
 document.getElementById('modalClose')
     .addEventListener('click', closeModal)
+
+document.getElementById('salvar')
+    .addEventListener('click', saveClient)
